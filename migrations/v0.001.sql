f0e31898f587b0dcfd06f6802c96394fb18ad3d6
Fatalwgx <98048609+Fatalwgx@users.noreply.github.com>
2023-03-09 16:42:49 +0300

    initial version

--- migrations/v0.001.sql
+++ migrations/v0.001.sql
@@ -3,17 +3,17 @@ create schema if not exists app;
 set schema 'app';
 
 create table if not exists accounts (
-		user_id serial primary key,
-		username VARCHAR(50) unique not null,
-		password VARCHAR(50) not null,
-		email VARCHAR(50) unique not null,
-		created_on TIMESTAMP not null,
-		last_login TIMESTAMP
+	user_id serial primary key,
+	username VARCHAR(50) unique not null,
+	password VARCHAR(50) not null,
+	email VARCHAR(50) unique not null,
+	created_on TIMESTAMP not null,
+	last_login TIMESTAMP
 );
 
 create table if not exists roles (
-		role_id serial primary key,
-		role_name VARCHAR(50) unique not null
+	role_id serial primary key,
+	role_name VARCHAR(50) unique not null
 );
 
 create table if not exists account_roles (
