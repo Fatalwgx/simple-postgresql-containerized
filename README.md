ddad9789becd66584cd729595d7fcbdb289e3de2
unknown <nothingfatalleo@gmail.com>
2023-03-09 16:26:49 +0300

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# simple-postgresql-containerized
+# Plain postgresql containerized
+
+A simple docker container containing postgresql with a few pre-initialized tables and basic data persistence using volumes
+
+## How To Run
+Make sure docker is installed
+1. Run 'docker-compose up' in terminal
+
+- Data will be persisted so long as volumes responsible for it aren't removed
+- Docker will automatically execute any .sql or .sh file in ./migrations/ directory
+- Script at ./migrations/v0.001.sql will initialize basic database structure
+- Connect to localhost:5432 using any sql client or ORM
---
+++ migrations/v0.001.sql
@@ -0,0 +1,28 @@
+create schema if not exists app;
+
+set schema 'app';
+
+create table if not exists accounts (
+		user_id serial primary key,
+		username VARCHAR(50) unique not null,
+		password VARCHAR(50) not null,
+		email VARCHAR(50) unique not null,
+		created_on TIMESTAMP not null,
+		last_login TIMESTAMP
+);
+
+create table if not exists roles (
+		role_id serial primary key,
+		role_name VARCHAR(50) unique not null
+);
+
+create table if not exists account_roles (
+	user_id INT not null,
+	role_id INT not null,
+	grant_date TIMESTAMP,
+	primary key (user_id, role_id),
+	foreign key (role_id)
+		references roles (role_id),
+	foreign key (user_id)
+		references accounts (user_id)
+);
